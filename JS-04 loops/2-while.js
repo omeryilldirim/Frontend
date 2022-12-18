f082abe8b57bb1db4719dd504cc7df0a3f06a9f1
omeryilldirim <omeryilldirim@outlook.com>
2022-12-18 15:00:00 +0300

    loops

---
+++ JS-04 loops/2-while.js
@@ -0,0 +1,15 @@
+// * ============================================
+// *            DONGULER(LOOPS)- WHILE
+// * ============================================
+
+console.log("****** WHILE *******")
+
+let count = 1
+while (count <= 10) {
+  console.log("Count:", count)
+  count++
+}
+
+//? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu while dongusu ile yaziniz.
