6aa1555c88857b2e76938dec96318f25c0b555eb
omeryilldirim <omeryilldirim@outlook.com>
2022-12-19 16:53:34 +0300

    loops practice

--- Assignment/JS_loops/practice.js
+++ Assignment/JS_loops/practice.js
@@ -78,7 +78,7 @@
 //   i++;
 // } while (i < 10);
 
-// //? d0...while : ask the user to input a grade and if the grade is not between 0-100 ask the user to input again and print how is the grade result
+// //? do...while : ask the user to input a grade and if the grade is not between 0-100 ask the user to input again and print how is the grade result
 
 // do {
 //   var grade = Number(prompt("Enter your grade"));
@@ -94,27 +94,81 @@
 //   console.log("you f.cked up!");
 // }
 
-//? create a do...while loop to ask the user to input grade until user input q or Q. when the user input q or Q stop the loop and sum up all the number inputed so far.
+// //? create a do...while loop to ask the user to input grade until user input q or Q. when the user input q or Q stop the loop and print the average of the numbers inputed so far.
 
-var sum = 0;
-var number = 0;
+// let sum = 0;
+// let number = 0;
+// let counter = 0;
 
-do {
-  sum += +number;
-  console.log(sum);
-  number = Number(prompt("enter a number to sum up or press q/Q to quit"));
-} while (String(number) !== "q" || String(number) !== "Q");
+// do {
+//   sum += +number;
+//   console.log(sum);
+//   number = prompt("enter a number to sum up or press q/Q to quit");
+//   if (number.toLowerCase() != "q") {
+//     counter += 1;
+//   }
+// } while (number.toLowerCase() != "q");
 
-console.log(`Sum is: ${sum}`);
+// console.log(`Average is: ${sum / counter}`);
 
-// # ODEV1: Console'dan girilen sayilarin ortalamasini bulan programi yaziniz. Program ilk olarak kullanicidan kac tane sayi gireceginin almali bu bilgi isiginda girilen sayilarin ortalamasini hesaplayarak yazdirmalidir.
+// //? create random number between 0-100 and user has 5 chances to guess. decrement chance counter by every guess and tell the user to raise or lower the number to guess right. if the user cant guess right print SORRY, if guessed right print CONGRATS.
 
-// # ODEV2: Console'dan girilen taban ve pozitif us degerlerine gore uslu sayi sonucunu hesaplayan kodu for veya while dongusu ile yaziniz.
+// const number = Math.trunc(Math.random() * 100);
+// let chance = 5;
+// alert(
+//   "Welcome to the GUESS game.You have 5 chances to guess the number right!"
+// );
 
-// # ODEV3 (ZOR): Console'dan girilen bir sayinin basamak sayisini hesaplayarak console'a bastiran kodu donguler yardimi ile yaziniz.
+// do {
+//   var guess = Number(prompt("Enter a number"));
+//   if (guess == number) {
+//     alert("Congrats! You guessed right!");
+//   } else {
+//     chance--;
+//     alert(`You have ${chance} chance left.`);
+//     if (chance == 0) {
+//       alert(
+//         `SORRY! You did not guess the number right! The Number is ${number} !!`
+//       );
+//       break;
+//     } else if (guess > number) {
+//       alert("You need to guess a lower number");
+//     } else {
+//       alert("You need to guess a higher number");
+//     }
+//   }
+// } while (guess != number);
 
-// ## ORNEK:
+// //? # ODEV1: Console'dan girilen sayilarin ortalamasini bulan programi yaziniz. Program ilk olarak kullanicidan kac tane sayi gireceginin almali bu bilgi isiginda girilen sayilarin ortalamasini hesaplayarak yazdirmalidir.
 
-// ### 43567 => Basamak sayisi: 5
+// alert("Average Calculator");
+// const count = Number(prompt("How many numbers will you input ?"));
+// let sum = 0;
 
-// ### 123 => Basamak sayisi: 3
+// for (let i = count; i != 0; i--) {
+//   const number = Number(prompt("Enter a number"));
+//   sum += number;
+// }
+// alert(`The average is ${sum / count}`);
+
+// //? # ODEV2: Console'dan girilen taban ve pozitif us degerlerine gore uslu sayi sonucunu hesaplayan kodu for veya while dongusu ile yaziniz.
+
+// const baseNum = Number(prompt("Enter base number"));
+// const exponentialNum = Number(prompt("Enter exponential number"));
+
+// alert(`The result is : ${baseNum ** exponentialNum}`);
+// console.log(`The result is : ${baseNum ** exponentialNum}`);
+
+// //? # ODEV3 (ZOR): Console'dan girilen bir sayinin basamak sayisini hesaplayarak console'a bastiran kodu donguler yardimi ile yaziniz.
+
+// const number = prompt("Enter a number");
+// let count = 0;
+
+// if (isNaN(number)) {
+//   alert("You did not enter number");
+// } else {
+//   for (let i = 1; i <= number.length; i++) {
+//     count++;
+//   }
+//   alert(`${count} digits number`);
+// }
