c496c19973166126ff1283c8724bb4cc663bfb57
omeryilldirim <omeryilldirim@outlook.com>
2022-12-22 20:38:29 +0300

    js function

---
+++ JS-06 function1/function.js
@@ -0,0 +1,10 @@
+// ? =====================================
+// ?           FUNCTIONS
+// ? ======================================
+
+console.log("** FUNC DECLARATION **");
+
+function greet() {
+  console.log("Hello, world!");
+}
+greet();
