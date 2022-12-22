685f9b3be2f79b78967dfdd0fb06590d63399d4c
omeryilldirim <omeryilldirim@outlook.com>
2022-12-22 21:00:24 +0300

    JS function

--- JS-06 function1/function.js
+++ JS-06 function1/function.js
@@ -4,7 +4,25 @@
 
 console.log("** FUNC DECLARATION **");
 
-function greet() {
+//* ORNEK1:
+//*****************************************/
+
+function printHello() {
   console.log("Hello, world!");
 }
-greet();
+printHello();
+
+//* ORNEK2:
+//*****************************************/
+
+function greet(firstName, lastName = "") {
+  console.log(`Hello ${firstName} ${lastName}`);
+}
+const name = prompt("Enter your name");
+const surname = prompt("Enter your surname");
+
+greet(name, surname);
+//! JS de fonksiyonlar parametrelere değişkenleri değil değerlerini aktarır. PASS BY VALUE
+
+greet("ahmet");
+//!Default parameter assignment ! lastName = "" olarak default değer atanabilir.
