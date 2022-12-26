aa1e5e74b23d7403f62f039c19be12b04e38b694
omeryilldirim <omeryilldirim@outlook.com>
2022-12-26 18:22:53 +0300

    js functions

--- Assignment/JS_functions/practice.js
+++ Assignment/JS_functions/practice.js
@@ -30,16 +30,45 @@
 
 //? function to check leap year
 
-const checkLeapYear = function (year) {
-  if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400) {
-    const result = `${year} is a leap year`;
-    return result;
-  } else {
-    const result = `${year} is a not leap year`;
-    return result;
+// const checkLeapYear = function (year) {
+//   if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400) {
+//     const result = `${year} is a leap year`;
+//     return result;
+//   } else {
+//     const result = `${year} is a not leap year`;
+//     return result;
+//   }
+// };
+
+// const year = prompt("Enter a year:");
+
+// console.log(checkLeapYear(year));
+
+//? Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+// For example (Input --> Output):
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+let perNum = prompt("Enter a positive number");
+
+while (perNum <= 0) {
+  perNum = prompt("Enter a positive number");
+}
+
+function persistence(num) {
+  let step = 0;
+  if (num.length > 1) {
+    do {
+      step += 1;
+      let result = 1;
+      for (i = 0; i < num.length; i++) {
+        result *= num[i];
+      }
+      num = String(result);
+    } while (num.length > 1);
   }
-};
+  return step;
+}
 
-const year = prompt("Enter a year:");
-
-console.log(checkLeapYear(year));
+console.log(persistence(perNum));
