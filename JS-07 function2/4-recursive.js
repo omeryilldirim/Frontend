f93210ef77d80d02851d0407c594d429daa12d48
omeryilldirim <omeryilldirim@outlook.com>
2022-12-26 23:05:25 +0300

    js functions

--- JS-07 function2/4-recursive.js
+++ JS-07 function2/4-recursive.js
@@ -10,7 +10,25 @@
 //* ORNEK: Girilen n. terime kadar Fibonacci sayilarinin toplamini
 //* hesaplayarak yazdiran fonksiyonu recursive olarak kodlayiniz.
 
-console.log("****** 4 - RECURSION ********")
+console.log("****** 4 - RECURSION ********");
 
 //? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, ...
 //! f(n) = f(n-1) + f(n-2)
+
+const fibo = (n) => {
+  if (n <= 0) {
+    return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    return fibo(n - 1) + fibo(n - 2);
+  }
+};
+
+const n = Number(prompt("Please enter n"));
+
+if (n <= 0) {
+  console.log("n should be bigger than 0");
+} else {
+  console.log(`FIBONACCI(${n}) = ${fibo(n)}`);
+}
