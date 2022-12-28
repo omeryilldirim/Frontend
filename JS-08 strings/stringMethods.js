ec4da1b9859bc1eefd920bcf7042d64c4b1c09a3
omeryilldirim <omeryilldirim@outlook.com>
2022-12-28 23:03:02 +0300

    strings

--- JS-08 strings/stringMethods.js
+++ JS-08 strings/stringMethods.js
@@ -27,3 +27,37 @@ console.log(`Hello
 world`);
 
 console.log(str1.slice(-5, 8));
+console.log(str1.substring(-8, -4));
+console.log(str1.substring(2, -4));
+console.log(str1.substring(0, 2));
+//! Any argument value that is less than 0 or greater than str.length is treated as if it were 0 and str.length, respectively.
+//! Any argument value that is NaN is treated as if it were 0.
+
+console.log(str1.includes("w"));
+console.log(str1.indexOf("W"));
+console.log(str1.indexOf("w", 3));
+
+const email = "omer@gmail.com"
+console.log(email.indexOf(".com", email.indexOf('@')));
+
+const str5 = "You do not KNOW what you do not know until you know."
+console.log(str5.search("know"));
+console.log(str5.search(/know/));
+console.log(str5.search(/know/i));
+
+const str6 = str5.replace("know", "learn")
+const str7 = str5.replace(/know/i, "learn")
+const str8 = str5.replace(/know/ig, "learn")
+const str9 = str5.replaceAll("know", "learn")
+console.log(str6);
+console.log(str7);
+console.log(str8);
+console.log(str9);
+
+const str10 = "Welcome to : Full Stack Clarusway"
+console.log(str10.split());
+console.log(str10.split(""));
+console.log(str10.split(" "));
+console.log(str10.split(","));
+console.log(str10.split(": "));
+console.log(str10.split("", 10));
