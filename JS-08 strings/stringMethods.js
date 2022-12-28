a4ebb7048bdc1a94c9f27e7602461e01fbbf208a
omeryilldirim <omeryilldirim@outlook.com>
2022-12-28 19:49:31 +0300

    JS strings

---
+++ JS-08 strings/stringMethods.js
@@ -0,0 +1,7 @@
+// ?======================================================
+// ?                     STRING METOTLARI
+// ! KAYNAK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// ?======================================================
+
+console.log("*** STRING METHODS ***");
+
