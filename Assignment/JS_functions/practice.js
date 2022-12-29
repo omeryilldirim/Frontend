5bccd8055953861b4b1218fc9abd12f45c2b1741
omeryilldirim <omeryilldirim@outlook.com>
2022-12-29 19:40:15 +0300

    functions

--- Assignment/JS_functions/practice.js
+++ Assignment/JS_functions/practice.js
@@ -73,7 +73,7 @@
 
 // console.log(persistence(perNum));
 
-//! *******************  JS CLASS EXAMPLES  ********************** 
+//! *******************  JS CLASS EXAMPLES  **********************
 
 //? 1- Celsius değerini Fahrenheit değerine dönüştüren bir fonksiyon yazınız? (Celsius × 9/5) + 32 = Fahrenheit
 //? console.log(calFahrenheit(30)) // output: 86
@@ -84,7 +84,6 @@
 // }
 // console.log(convert(30));
 // console.log(convert(23));
-
 
 //? 2- Belirli bir string ifadenin tersini bulan fonksiyonu yazınız?
 //? console.log(reverseString('hello world')) // output: dlrow olleh
@@ -97,9 +96,8 @@
 //   console.log(result);
 // }
 
-// reverseString("hello world")
+// reverseString("Hello world!")
 // reverseString("how are you")
-
 
 //? 3- Belirli bir string ifadenin palindrome olup olmadığını bulan fonksiyonu yazınız?(Büyük , küçük harf duyarlılığı olmamalıdır.)
 //? const string = 'selles'
@@ -114,7 +112,7 @@
 // }
 
 // console.log(isPalindrome("selles"));
-// console.log(isPalindrome("hello")); 
+// console.log(isPalindrome("hello"));
 
 //? 4- Belirli bir string ifadenin içindeki sesli harfleri bulan fonksiyonu yazınız?
 //? console.log(findVowels('hello world')) // output: eoo
@@ -183,3 +181,78 @@
 // console.log(isPrime(1));
 // console.log(isPrime(-50));
 // console.log(isPrime(97));
+
+//! *******************  EXERCISE  **********************
+
+// //? Exercise-5
+// //? Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+// //? pyramid(0) => [ ]
+// //? pyramid(1) => [ [1] ]
+// //? pyramid(2) => [ [1], [1, 1] ]
+// //? pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+// //? Note: the subarrays should be filled with 1s
+
+// function pyramid(n) {
+//   let arr = [];
+  
+//   for (i = 1; i <= n; i++) {
+//     let subArr = [];
+
+//     for (j=1; j<=i; j++){
+//         subArr.push(j)
+//     }
+
+//     subArr.fill(1)
+//     arr.push(subArr)
+//   }
+//   return arr;
+// }
+
+// console.log(pyramid(4));
+
+//? CODEWARS basic mathematical operations
+//? Your task is to create a function that does four basic mathematical operations.
+//? The function should take three arguments - operation(string/char), value1(number), value2(number).
+//? The function should return result of numbers after applying the chosen operation.
+//? Examples(Operator, value1, value2) --> output
+//? ('+', 4, 7) --> 11
+//? ('-', 15, 18) --> -3
+
+// const calculate = function (operator, num1, num2) {
+//     let result = 0
+//     switch (operator) {
+//         case "+":
+//             result = num1 + num2
+//             break;
+//         case "-":
+//             result = num1 - num2
+//             break;
+//         case "*":
+//             result = num1 * num2
+//             break;
+//         case "/":
+//             result = num1 / num2
+//             break;
+//         default:
+//             return `invalid operator`
+//     }
+//     return result
+// }
+
+// console.log(calculate("*", 5, 6));
+
+//! Teamwork 06 JS assignment
+//? Write a code that given a string, returns reverse orders of words
+//? "Clarusway" => "Clarusway"
+//? "days. big make things Little" => "Little things make big days."
+
+const reverseWords = str => {
+    const splitted = str.split(' ')
+    let newStr = ""
+    for (let i = (splitted.length)-1; i >= 0 ; i--)
+        newStr += splitted[i] + " "
+    return newStr
+}
+
+console.log(reverseWords("Clarusway"));
+console.log(reverseWords("days. big make things Little"));
