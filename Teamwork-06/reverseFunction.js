b8ef57704325ec4624783b9498ddbad3b063a2db
omeryilldirim <omeryilldirim@outlook.com>
2022-12-30 22:22:32 +0300

    website design

--- Teamwork-06/reverseFunction.js
+++ Teamwork-06/reverseFunction.js
@@ -1,30 +1,30 @@
-//! Teamwork 06 JS assignment
+// //! Teamwork 06 JS assignment
 
-//? 2- Write a function which returns reverse order of the string.
-//? console.log(reverseString('hello world')) // output: dlrow olleh
+// //? 2- Write a function which returns reverse order of the string.
+// //? console.log(reverseString('hello world')) // output: dlrow olleh
 
-const reverseString = function(str){
-  let  result = ""
-  for (i=(str.length)-1 ; i>=0 ; i-- ){
-    result += str[i]
-  }
-  console.log(result);
-}
+// const reverseString = function(str){
+//   let  result = ""
+//   for (i=(str.length)-1 ; i>=0 ; i-- ){
+//     result += str[i]
+//   }
+//   console.log(result);
+// }
 
-reverseString("Hello world!")
-reverseString("how are you")
+// reverseString("Hello world!")
+// reverseString("how are you")
 
-//? Write a code that given a string, returns reverse orders of words
-//? "Clarusway" => "Clarusway"
-//? "days. big make things Little" => "Little things make big days."
+// //? Write a code that given a string, returns reverse orders of words
+// //? "Clarusway" => "Clarusway"
+// //? "days. big make things Little" => "Little things make big days."
 
-const reverseWords = str => {
-    const splitted = str.split(' ')
-    let newStr = ""
-    for (let i = (splitted.length)-1; i >= 0 ; i--)
-        newStr += splitted[i] + " "
-    return newStr
-}
+// const reverseWords = str => {
+//     const splitted = str.split(' ')
+//     let newStr = ""
+//     for (let i = (splitted.length)-1; i >= 0 ; i--)
+//         newStr += splitted[i] + " "
+//     return newStr
+// }
 
-console.log(reverseWords("Clarusway"));
-console.log(reverseWords("days. big make things Little"));
+// console.log(reverseWords("Clarusway"));
+// console.log(reverseWords("days. big make things Little"));
