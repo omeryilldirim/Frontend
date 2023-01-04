3c5f5c2da86fa7fd3dcc22e5d916a8d727b0e86c
omeryilldirim <omeryilldirim@outlook.com>
2023-01-04 20:01:18 +0300

    array iteration

---
+++ JS-10 arrays_iteration/1-arraysMethods.js
@@ -0,0 +1,28 @@
+//? ===============================================
+//?           DIZI ERISIM METOTLARI
+//? ===============================================
+
+console.log("**** ACCESS ****")
+const numbers = [3, 5, 2, "4", "four", 3, "4", "bes", "1"]
+
+//* includes()
+//*-----------------------------------------------------------
+
+//* indexOf(), lastIndexOf();
+//*-----------------------------------------------------------
+//* ilk eslesen indeksi dondurur. Bulunumazsa -1 dondurur.
+
+//* join()
+//*-----------------------------------------------------------
+//? join, dizinin elamanlari birlestirip string'e cevirir.
+
+//* toString()
+//*-----------------------------------------------------------
+//? toString fonksiyonu sadece dizinin elemanlarinin aralarina
+//? (virgul) koyarak birlestirir ve string yapar.
+
+//* slice(start, end) -- end exclusive
+//*-----------------------------------------------------------
+
+//* concat()
+//*-----------------------------------------------------------
