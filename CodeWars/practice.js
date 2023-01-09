3c0e1ddc5e9ddfa160b02403fa61bcc3c8d42fd2
omeryilldirim <omeryilldirim@outlook.com>
2023-01-09 19:38:54 +0300

    codewars

--- CodeWars/practice.js
+++ CodeWars/practice.js
@@ -58,3 +58,21 @@ function persistence(num) {
  console.log(persistence(39));
  console.log(persistence(999));
  console.log(persistence(4));
+
+
+ //! ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN (pin) {
+    return (pin.length === 4 || pin.length === 6) && pin.split('').every(digit => /\d/.test(digit));
+  }
+
+console.log(validatePIN("1234"));
+console.log(validatePIN("12345"));
+console.log(validatePIN("a234"));
+
