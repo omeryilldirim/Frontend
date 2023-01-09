0fc6aff230d5be83c92028f295932c76b92341c7
omeryilldirim <omeryilldirim@outlook.com>
2023-01-09 21:24:00 +0300

    objects

--- JS-11 objects/objects.js
+++ JS-11 objects/objects.js
@@ -165,7 +165,73 @@ for (const i in arrPeople) {
   console.log(`${arrPeople[i][0]} salary : ${arrPeople[i][1].salary}`);
 }
 
+//! FOR - IN
+//* for (key in object) {
+//*   // code block to be executed
+//* }
+// ;("strict")
+for (let person in people) {
+  console.log(person)
+  console.log(people[person].salary)
+  console.log(people[person].name)
+}
+
+//! FOR - OF
+//* for (x of iterable) {
+//*   code block to be executed
+//* }
+
+//? Object methods
+console.log(Object.keys(people))
+console.log(Object.values(people))
+console.log(Object.entries(people))
+
+for (let key of Object.keys(people)) {
+  console.log(key)
+}
+
+for (let v of Object.values(people)) {
+  console.log(v)
+}
+
+for (let [k, v] of Object.entries(people)) {
+  console.log("KEY:", k, "VALUE:", v.job)
+}
+
+//! ARRAY METOTLARI ILE
+console.log("*********")
+Object.keys(people).forEach((p) => console.log(p))
+console.log("*********")
+
+Object.values(people).forEach((p) => console.log(p.name))
+//? Javascript'de Objeler default olarak iterable degildir.
+//? Ama for in ve for of donguleri ile itere edilebilirler.
+
+//? Objelerin key ve value'larini okumak icin built-in metotlar vardir.
+//? Bu metotlar aslinda objelerin key ve/veya value'lari bir dizi olarak dondurur.
+
+
 //? job'i developer olanlarin dob degerlerini yazdiriniz.
 
 const developers = arrPeople.filter((person) => person[1].job === "developer")
 developers.forEach((person) => console.log(`${person[0]} dob is : ${person[1].dob}`))
+
+Object.values(people)
+  .filter((p) => p.job === "developer")
+  .forEach((p) => console.log(p.dob))
+
+console.log("*****")
+const dobs = Object.values(people)
+  .filter((p) => p.job === "developer")
+  .map((p) => p.dob) //? (2) ['1990', '2000']
+
+console.log(dobs)
+
+//********************************************************
+//* JSON => Javascript Object Notation
+//********************************************************
+const team = [
+  { name: "Josh", surname: "Adams", job: "developer", age: 30 },
+  { name: "Mary", surname: "Bary", job: "tester", age: 22 },
+  { name: "Hazel", surname: "Nut", job: "developer", age: 20 },
+] //* JSON
