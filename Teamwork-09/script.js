7c36d36383e978a6fc6d5d34a66ec96ce43c990e
omeryilldirim <omeryilldirim@outlook.com>
2023-01-20 12:46:59 +0300

    teamwork

---
+++ Teamwork-09/script.js
@@ -0,0 +1,7 @@
+// Purpose of the this coding challenge is to write a code that given numbers convert to Roman Numerals. The Romans wrote numbers using letters - I, V, X, L, C, D, M. (notice these letters have lots of straight lines and are hence easy to hack into stone tablets).
+
+// Example:,
+// 1000=M 900=CM 90=XC
+// 2008 is written as MMVIII:
+// 2000=MM 8=VIII
+
