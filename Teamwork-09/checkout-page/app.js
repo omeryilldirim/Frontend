40cc5eb1340f2f846c63966d89c053649ad78d59
omeryilldirim <omeryilldirim@outlook.com>
2023-01-22 00:37:35 +0300

    checkout page

--- Teamwork-09/checkout-page/app.js
+++ Teamwork-09/checkout-page/app.js
@@ -1,14 +1,11 @@
-item1Obj = {id:"product1", name:"bag", price:25.98, qty:1, subtotal:45.99}
-item2Obj = {id:"product2", name:"shoes", price:45.99, qty:1, subtotal:45.99}
-item3Obj = {id:"product3", name:"clock", price:74.99, qty:1, subtotal:74.99}
-
+//set items and add to local storage
+item1Obj = {name:"bag", price:25.98, subtotal:25.98}
+item2Obj = {name:"shoes", price:45.99, subtotal:45.99}
+item3Obj = {name:"clock", price:74.99, subtotal:74.99}
 
 localStorage.setItem("item1", JSON.stringify(item1Obj))
 localStorage.setItem("item2", JSON.stringify(item2Obj))
 localStorage.setItem("item3", JSON.stringify(item3Obj))
-
-
-
 
 // get plus buttons
 const buttonsPlus = document.querySelectorAll("#btn-plus")
@@ -28,29 +25,35 @@ const buttonsRemove = document.querySelectorAll(".btn-remove")
 //add event to remove buttons
 buttonsRemove.forEach(btn=>btn.addEventListener("click", removeItem))
 
-
-
 function removeItem (e){
+    // get product element
     const product = e.target.closest("section")
+    // remove from display
     product.style.display = "none"
+    // get which item is removed
+    const item = e.target.closest("section").getAttribute("class")
+    // remove from localstorage
+    localStorage.removeItem(item)
+    updatePayment()
 }
 
 function decreaseQuantity (e) {
+    //get which item is revised
+    const item = e.target.closest("section").getAttribute("class")
     //get item quantity number
     const itemQty = e.target.nextElementSibling
     itemQty.innerText > 1 && (itemQty.innerText = itemQty.innerText - 1)
     //get item price for 1pcs
-    const itemPrice = +itemQty.parentNode.previousElementSibling.firstChild.innerHTML.substr(1)
+    // const itemPrice = +itemQty.parentNode.previousElementSibling.firstChild.innerHTML.substr(1)
+    const itemPrice = (JSON.parse(localStorage.getItem(item))).price
     //calculate item total price
     const itemTotalPrice = itemQty.parentNode.nextElementSibling.nextElementSibling.lastChild
     const newPrice = Number((itemPrice * +itemQty.innerText).toFixed(2))
     //update item total price
     itemTotalPrice.innerHTML = `$${newPrice}`
     // update local storage subtotal
-    //get which item is revised
-    const item = e.target.closest("section").getAttribute("class")
     //get the value from local storage
-    let storageObj  = (JSON.parse(localStorage.getItem(item)))
+    const storageObj  = (JSON.parse(localStorage.getItem(item)))
     //change the subtotal
     storageObj.subtotal = newPrice
     //send back to local storage
@@ -60,21 +63,22 @@ function decreaseQuantity (e) {
 }
 
 function increaseQuantity (e) {
+    //get which item is revised
+    const item = e.target.closest("section").getAttribute("class")
     //get item quantity number
     const itemQty = e.target.previousElementSibling
     itemQty.innerText = +itemQty.innerText + 1
     //get item price for 1pcs
-    const itemPrice = +itemQty.parentNode.previousElementSibling.firstChild.innerHTML.substr(1)
+    // const itemPrice = +itemQty.parentNode.previousElementSibling.firstChild.innerHTML.substr(1)
+    const itemPrice = (JSON.parse(localStorage.getItem(item))).price
     //calculate item total price
     const itemTotalPrice = itemQty.parentNode.nextElementSibling.nextElementSibling.lastChild
     const newPrice = Number((itemPrice * +itemQty.innerText).toFixed(2))
     //update item total price
     itemTotalPrice.innerHTML = `$${newPrice}`
     // update local storage subtotal
-    //get which item is revised
-    const item = e.target.closest("section").getAttribute("class")
     //get the value from local storage
-    let storageObj  = (JSON.parse(localStorage.getItem(item)))
+    const storageObj  = (JSON.parse(localStorage.getItem(item)))
     //change the subtotal
     storageObj.subtotal = newPrice
     //send back to local storage
@@ -87,10 +91,10 @@ function updatePayment(){
     const shippingPrice = 15
     // get subtotal element
     const subtotalEl = document.querySelector(".subtotal span:nth-child(2)")
-    // add 3 items prices
+    // add 3 items prices and only add if item is in cart so on localStorage
     let subtotalPrice = 0
     for (let i = 1; i < 4; i++) {
-        subtotalPrice += JSON.parse(localStorage.getItem(`item${i}`)).subtotal
+        localStorage.getItem(`item${i}`) && (subtotalPrice += JSON.parse(localStorage.getItem(`item${i}`)).subtotal)
     }
     // assign subtotal to dom
     subtotalEl.innerText = `$${subtotalPrice.toFixed(2)}`
@@ -104,8 +108,3 @@ function updatePayment(){
     // assign totalPrice to dom
     totalEl.innerText = `$${(subtotalPrice+taxPrice+shippingPrice).toFixed(2)}`
 }
-
-// load event vs DomContentLoaded
-// window.addEventListener("load", () => {
-//     getItemListFromLocalStorage();
-//   });
