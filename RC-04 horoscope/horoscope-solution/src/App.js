46eabe029fd81bcad516d1ebdca2a375ac46400a
omeryilldirim <omeryilldirim@outlook.com>
2023-02-16 21:38:21 +0300

    horoscope

--- RC-04 horoscope/horoscope-solution/src/App.js
+++ RC-04 horoscope/horoscope-solution/src/App.js
@@ -1,10 +1,13 @@
 import './App.scss';
 import Header from './components/Header/Header.jsx';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () =>{
   return (
     <div className="App">
+      <Navbar />
       <Header />
+      
     </div>
   )
 }
