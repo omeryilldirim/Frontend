4d27c689c76758948f599e876976f72b1d53b484
omeryilldirim <omeryilldirim@outlook.com>
2023-02-20 22:45:13 +0300

    keyboard events

--- RC-06_forms-otherEvents/src/App.js
+++ RC-06_forms-otherEvents/src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
-      <FormWithObject />
-      {/* <KeyboardEvents />
-      <MouseEvent /> */}
+      {/* <FormWithObject /> */}
+      <KeyboardEvents />
+      {/* <MouseEvent /> */}
     </div>
   )
 }
--- RC-06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ RC-06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+
 const KeyboardClipboard = () => {
+  
+  const [inputData, setInputData] = useState("")
+
+  const handleChange = (e) =>{
+    e.target.value = e.target.value.toUpperCase()
+    setInputData(e.target.value)
+  }
+  const handleKeyDown = (e) =>{
+    // console.log(e.keyCode);
+    if (e.keyCode >= 48 && e.keyCode <= 57){
+      e.preventDefault() //? prevents printing number
+    }
+  }
+  const handleAreaPaste = (e)=>{
+    e.target.style.color="red"
+    e.target.value += e.clipboardData.getData("text").toLocaleUpperCase()
+    e.preventDefault()
+  }
+  
   return (
     <div className="container text-center">
-      <h2>Keyboard-Clipboard Event</h2>
+      <h2 className="display-5 text-danger">Keyboard-Clipboard Event</h2>
+
+      <input type="text" className="form-control" onChange={handleChange} onKeyDown={handleKeyDown} />
+
+      <div className="text-start mt-4">
+        <h6>Copied Input Data</h6>
+        <p>{inputData.toLowerCase()}</p>
+      </div>
+
+      <textarea className="form-control" name="textarea" id="textarea" cols="30" rows="10" onPaste={handleAreaPaste}></textarea>
     </div>
   )
 }
