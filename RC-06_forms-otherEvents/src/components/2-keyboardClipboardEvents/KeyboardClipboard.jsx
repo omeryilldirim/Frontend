b55fdbe4bcbba2f7b5cf0732ed32366a90c4abc6
omeryilldirim <omeryilldirim@outlook.com>
2023-02-20 20:08:29 +0300

    rc 06

---
+++ RC-06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -0,0 +1,9 @@
+const KeyboardClipboard = () => {
+  return (
+    <div className="container text-center">
+      <h2>Keyboard-Clipboard Event</h2>
+    </div>
+  )
+}
+
+export default KeyboardClipboard
