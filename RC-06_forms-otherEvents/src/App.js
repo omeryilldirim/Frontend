709e4cfe43e9876172c46de326298139fc70af63
omeryilldirim <omeryilldirim@outlook.com>
2023-02-21 20:12:08 +0300

    rc06

--- RC-06_forms-otherEvents/src/App.js
+++ RC-06_forms-otherEvents/src/App.js
@@ -8,8 +8,8 @@ function App() {
     <div className="container mt-4 text-center">
       {/* <Form /> */}
       {/* <FormWithObject /> */}
-      <KeyboardEvents />
-      {/* <MouseEvent /> */}
+      {/* <KeyboardEvents /> */}
+      <MouseEvent />
     </div>
   )
 }
