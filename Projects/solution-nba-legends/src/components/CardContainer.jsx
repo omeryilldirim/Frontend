8bd6123846e8c70054234dd36ce2cc7972a8aafd
omeryilldirim <omeryilldirim@outlook.com>
2023-02-21 21:47:15 +0300

    nba legends

--- Projects/solution-nba-legends/src/components/CardContainer.jsx
+++ Projects/solution-nba-legends/src/components/CardContainer.jsx
@@ -1,13 +1,19 @@
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import {data} from "../helpers/data"
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
   return (
     <Container className="card-container rounded-4 my-4 p-3">
-        {data.map((player)=> (
-        <PlayerCard {...player}/>
-        ))}
+        <Row>
+            {data.map((player, index)=> (
+                <Col key={index}>
+                    <PlayerCard {...player}/>
+                </Col>
+            ))}
+        </Row>
     </Container>
   )
 }
