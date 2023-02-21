ff8132adcd7347379eee4dfdf70c680b4f2f9f3e
omeryilldirim <omeryilldirim@outlook.com>
2023-02-21 22:55:52 +0300

    nba legends

--- Projects/solution-nba-legends/src/components/CardContainer.jsx
+++ Projects/solution-nba-legends/src/components/CardContainer.jsx
@@ -1,21 +1,39 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {data} from "../helpers/data"
+import Form from "react-bootstrap/Form";
+import { data } from "../helpers/data";
 import PlayerCard from "./PlayerCard";
+import { useState } from "react";
 
 const CardContainer = () => {
+  const [search, setSearch] = useState("");
   return (
-    <Container className="card-container rounded-4 my-4 p-3">
-        <Row>
-            {data.map((player, index)=> (
-                <Col key={index}>
-                    <PlayerCard {...player}/>
-                </Col>
-            ))}
-        </Row>
-    </Container>
-  )
-}
+    <>
+      <Form.Control
+        className="w-50 m-auto"
+        type="search"
+        placeholder="Search player..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
-export default CardContainer
+<Container className="card-container rounded-4 my-4 p-3">
+        <Row className="g-3 justify-content-center">
+          {data
+            .filter((player) =>
+              player.name.toLowerCase().includes(search.trim().toLowerCase())
+            )
+            .map((player, i) => {
+              return (
+                <Col md={6} lg={4} xl={3} key={i}>
+                  <PlayerCard {...player} />
+                </Col>
+              );
+            })}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default CardContainer;
--- Projects/solution-nba-legends/src/components/PlayerCard.jsx
+++ Projects/solution-nba-legends/src/components/PlayerCard.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 
 
 const PlayerCard = ({name, img, statistics}) => {
-    // const {name,img,statistics} = props
+  const [showImage, setShowImage] = useState(true)
+
+  // const {name,img,statistics} = props
+  
   return (
-    <Card >
-      <Card.Img variant="top" src={img} />
+    <Card onClick={()=>setShowImage(!showImage)} className='rounded-2 m-auto player-card' role="button">
+      {showImage ? 
+        (<Card.Img variant="top" src={img} className="player-img"/>
+        ) : (
+        <ul className='m-auto'>{statistics.map((item,i)=>{
+            return (
+                <li className='h5 text-start list-unstyled' key={i}> 🏀 {item}</li>
+            )
+        })}
+        </ul>
+        )}
       <Card.Footer>
         <Card.Title>{name}</Card.Title>
       </Card.Footer>
