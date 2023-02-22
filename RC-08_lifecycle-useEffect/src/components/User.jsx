a459f8a058bfe5a00ca2164d24d48a063f63c845
omeryilldirim <omeryilldirim@outlook.com>
2023-02-22 22:59:33 +0300

    useEffect

--- RC-08_lifecycle-useEffect/src/components/User.jsx
+++ RC-08_lifecycle-useEffect/src/components/User.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const User = () => {
-  return (
-    <div>User</div>
-  )
-}
+  const [user, setUser] = useState("")
+  
+  const getUser = () => {
+    fetch("https://randomuser.me/api/")
+    .then((res) => res.json())
+    .then((data) => setUser(data.results[0]))
+    .catch((err) => console.log(err));
+  }
 
-export default User
+  useEffect(() => {
+    console.log("mounting")
+    getUser()
+  }, [])
+  
+  console.log("render");
+  console.log(user);
+
+  return (
+    <div>
+      <h1>{user?.name?.first} {user?.name?.last}</h1>
+      <img className="rounded-circle" src={user?.picture?.large} alt="img" />
+      <h3>{user?.location?.country}, {user?.nat}</h3>
+      <h4>{user?.email}</h4>
+      <h5>{user?.phone}</h5>
+      <p>age : {user?.dob?.age}</p>
+
+      <button className="btn btn-warning" onClick={getUser}>Get User</button>
+    </div>
+  );
+};
+
+export default User;
