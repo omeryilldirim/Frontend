643e2def4fdfa2bf8fbd72d081e1cdf9a13453a5
omeryilldirim <omeryilldirim@outlook.com>
2023-02-25 16:19:15 +0300

    edit tutorial

--- RC-09_useeffect-axios-example/tutorial-app-inClass/src/components/EditTutorial.jsx
+++ RC-09_useeffect-axios-example/tutorial-app-inClass/src/components/EditTutorial.jsx
@@ -2,34 +2,29 @@ import { useEffect, useState } from "react";
 
 
 const EditTutorial = ({getTutorials, info}) => {
-  const [title, setTitle] = useState("")
-  const [desc, setDesc] = useState("")
-  const [tutor, setTutor] = useState(info)
+  const [title, setTitle] = useState(info.title)
+  const [desc, setDesc] = useState(info.description)
+  // const [tutor, setTutor] = useState(info)
 
   // tutor !== info && setTutor(info)
 
   useEffect(() => {
-    setTutor(info)
-  
-    return () => {
 
-  }}, [info])
+    setTitle(info.title)
+    setDesc(info.description)
+    
+  }, [info])
 
   const handleSaveChanges = (e)=>{
     ;
   }
   const handleTitleChange = (e)=>{
     setTitle(e.target.value)
-    console.log(title);
   }
   const handleDescriptionChange = (e)=>{
     setDesc(e.target.value)
-    console.log(desc);
   }
-  const handleClose = () => {
-    setTitle("")
-    setDesc("")
-  }
+
 
   return (
     <div className="modal" tabIndex="-1" id="edit-tutor">
@@ -47,11 +42,11 @@ const EditTutorial = ({getTutorials, info}) => {
           <div className="modal-body">
             <form action="#">
               <label className="form-label" htmlFor="title">Title</label>
-              <input className="form-control mb-3" type="text" name="title" id="title" value={info.title || ""} 
+              <input className="form-control mb-3" type="text" name="title" id="title" value={title || ""} 
               onChange={handleTitleChange}
               />
               <label className="form-label"  htmlFor="desc">Description</label>
-              <input className="form-control" type="text" name="desc" id="desc" defaultValue={info.description || ""}
+              <input className="form-control" type="text" name="desc" id="desc" value={desc || ""}
               onChange={handleDescriptionChange}
                />
             </form>
@@ -61,7 +56,7 @@ const EditTutorial = ({getTutorials, info}) => {
               type="button"
               className="btn btn-secondary"
               data-bs-dismiss="modal"
-              onClick={handleClose}
+
             >
               Close
             </button>
