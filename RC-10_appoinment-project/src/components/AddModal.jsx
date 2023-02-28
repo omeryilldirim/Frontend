2d67335cc72a7d31df7f2df5e3c6c477c4cd3736
omeryilldirim <omeryilldirim@outlook.com>
2023-02-28 13:36:54 +0300

    hospital app

---
+++ RC-10_appoinment-project/src/components/AddModal.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react"
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+
+export function AddModal({
+  show,
+  handleClose,
+  drName,
+  appointments,
+  setAppointments,
+}) {
+  const [patientName, setPatientName] = useState("")
+  const [date, setDate] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setAppointments([
+      ...appointments,
+      {
+        id: appointments.length + 1,
+        patient: patientName,
+        day: date,
+        consulted: false,
+        doctor: drName,
+      },
+    ])
+    handleClose()
+  }
+
+  console.log(appointments)
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Appointment for {drName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your name"
+                onChange={(e) => setPatientName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="datetime">
+              <Form.Label>Day&Time</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </Form.Group>
+
+            <div className="text-center">
+              <Button variant="success" type="submit" className="me-2">
+                Save
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  )
+}
---
+++ RC-10_appoinment-project/src/components/Doctors.jsx
@@ -0,0 +1,53 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import { AddModal } from "./AddModal"
+import { useState } from "react"
+
+const Doctors = ({ doctors, appointments, setAppointments }) => {
+  const [show, setShow] = useState(false)
+  const [selectedDrName, setSelectedDrName] = useState("")
+
+  // const handleClose = () => setShow(false)
+  // const handleShow = () => setShow(true)
+
+  const handleClick = (drName) => {
+    // handleShow()
+    setShow(true)
+    setSelectedDrName(drName)
+  }
+
+  console.log(selectedDrName)
+  return (
+    <Container className="p-2">
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
+        Our Doctors
+      </h3>
+
+      <Row className="justify-content-center">
+        {doctors.map((dr) => (
+          <Col key={dr.id} xs={6} sm={4} md={3}>
+            <img
+              src={dr.img}
+              alt={dr.name}
+              className="img-thumbnail doctor-img"
+              onClick={() => handleClick(dr.name)}
+            />
+            <h5>{dr.name}</h5>
+            <h6>{dr.dep}</h6>
+          </Col>
+        ))}
+      </Row>
+      {/* <AddModal show={show} handleClose={handleClose} /> */}
+      <AddModal
+        show={show}
+        handleClose={() => setShow(false)}
+        drName={selectedDrName}
+        appointments={appointments}
+        setAppointments={setAppointments}
+      />
+    </Container>
+  )
+}
+
+export default Doctors
