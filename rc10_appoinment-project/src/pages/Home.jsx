315fee83ae5b457c07172932973860df7efef97e
omeryilldirim <omeryilldirim@outlook.com>
2023-02-28 17:02:20 +0300

    appointment app

---
+++ rc10_appoinment-project/src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import AppointmentList from "../components/AppointmentList"
+import Doctors from "../components/Doctors"
+import { doctorData } from "../helper/data"
+import { appointmentData } from "../helper/data"
+
+const Home = () => {
+  const [doctors, setDoctors] = useState(doctorData)
+  const [appointments, setAppointments] = useState(appointmentData)
+
+  //! Mock Data yerine API'Den veri cekilseydi
+  // const getAppointments = async () => {
+  //   try {
+  //     const { data } = await axios.get("url")
+  //     setAppointments(data)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getAppointments()
+  // }, [])
+
+  console.log(appointments)
+  console.log(doctors)
+  return (
+    <main className="text-center mt-2">
+      <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
+      <Doctors
+        doctors={doctors}
+        appointments={appointments}
+        setAppointments={setAppointments}
+      />
+      <AppointmentList
+        appointments={appointments}
+        setAppointments={setAppointments}
+      />
+    </main>
+  )
+}
+
+export default Home
