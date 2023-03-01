45816705b6eb6efa6ef65211647e642efc31bfa8
omeryilldirim <omeryilldirim@outlook.com>
2023-03-01 15:07:41 +0300

    error handled

--- rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -7,30 +7,46 @@ const PersonDetail = () => {
   //! Bu durumda veri, state ile geldigi icin yeniden fetch yapilmasina gerek kalmaz
   // const { state: person } = useLocation()
   const navigate = useNavigate()
+  const [isPersonExist, setIsPersonExist] = useState(true)
 
   //! Linkteki parametreyi almak icin useParams Hook'u kullanilabilir.
   const { id } = useParams()
   const [person, setPerson] = useState({})
 
   const getPerson = () => {
-    fetch(`https://reqres.in/api/users/${id}`)
-      .then((res) => res.json())
+    try {
+      fetch(`https://reqres.in/api/users/${id}`)
+      .then((res) => {
+         if (res.status >= 400){
+          throw new Error('User fault');
+         }
+        res.json()})
       .then((data) => setPerson(data.data))
-      .catch((err) => console.log(err))
+      .catch((err) => setIsPersonExist(false))
+    } catch (error) {
+      console.log(error);
+      setIsPersonExist(false)
+    }
   }
   useEffect(() => {
     getPerson()
   }, [])
 
-  console.log(id)
 
   return (
     <div className="container text-center">
-      <h3>
-        {person?.first_name} {person?.last_name}
-      </h3>
-      <img className="rounded" src={person?.avatar} alt="" />
-      <p>{person?.email}</p>
+      {isPersonExist ? (
+        <div>
+          <p>person</p>
+            <h3>
+              {person?.first_name} {person?.last_name}
+            </h3>
+            <img className="rounded" src={person?.avatar} alt="" />
+            <p>{person?.email}</p>
+        </div>
+      ):(
+        <div className="m-5 bg-danger text-light p-5">This person does not exist</div>
+      )}
       <div>
         <button onClick={() => navigate("/")} className="btn btn-success me-2">
           Go Home
