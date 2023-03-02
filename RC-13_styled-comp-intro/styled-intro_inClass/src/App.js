9c8383fe15bc3dda9587a6dac006dda429c49987
omeryilldirim <omeryilldirim@outlook.com>
2023-03-02 21:28:30 +0300

    styled components

--- RC-13_styled-comp-intro/styled-intro_inClass/src/App.js
+++ RC-13_styled-comp-intro/styled-intro_inClass/src/App.js
@@ -1,7 +1,13 @@
+import { Button, FS13Button } from "./components/Button.styled";
+
 const App = () => {
   return (
     <>
       <h1>Styled Components</h1>
+      <Button>Send</Button>
+      <Button primary>Close</Button>
+      <FS13Button react>STUDY</FS13Button>
+      <FS13Button>REST</FS13Button>
     </>
   );
 };
---
+++ RC-13_styled-comp-intro/styled-intro_inClass/src/components/Button.styled.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    /* color:white;
+    background-color:hotpink; */
+    background-color: ${props => props.primary ? "white" : "hotpink"};
+    color: ${({primary}) => primary ? "hotpink" : "white"};
+    width:8rem;
+    margin: 1rem;
+    padding: 1rem 2rem;
+    border-radius:1rem;
+    border:2px solid hotpink;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 1.2rem;
+    transition: all 0.2s ease-in;
+    &:hover{
+        transform: scale(0.95);
+        cursor:pointer;
+    }
+    `
+
+export const FS13Button = styled(Button)`
+  background-color: ${({ react }) => (react ? "red" : "white")};
+  color: ${({ react }) => (react ? "white" : "red")};
+  border-color: ${({ react }) => (react ? "black" : "red")};
+`
