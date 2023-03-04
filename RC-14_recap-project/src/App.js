0a7b5248ea58dd018d2a309ed0b7061c79bfe634
omeryilldirim <omeryilldirim@outlook.com>
2023-03-04 11:07:00 +0300

    recap

---
+++ RC-14_recap-project/src/App.js
@@ -0,0 +1,23 @@
+
+import Main from "./pages/Main";
+import NewProduct from "./pages/NewProduct";
+import ProductList from "./pages/ProductList";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
+function App() {
+
+  
+
+  return (
+    <div className="bg-light app">
+      <Navbar/>
+      <Main/>
+      <About/>
+      <NewProduct/>
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
