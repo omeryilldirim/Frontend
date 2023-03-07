73792159cb2db4afc7acc3e641f8cd5e56e5cd85
omeryilldirim <omeryilldirim@outlook.com>
2023-03-07 21:01:30 +0300

    routers added

--- RC-15-16_clarus-library/clarus-library-inclass/src/router/AppRouter.jsx
+++ RC-15-16_clarus-library/clarus-library-inclass/src/router/AppRouter.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Navbar from "../components/navbar/Navbar"
+import Footer from "../components/footer/Footer"
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import PrivateRouter from "./PrivateRouter";
+import Detail from "../pages/detail/Detail";
+import About from "../pages/about/About";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="/detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+
   )
 }
 
--- RC-15-16_clarus-library/clarus-library-inclass/src/styles/theme.js
+++ RC-15-16_clarus-library/clarus-library-inclass/src/styles/theme.js
@@ -1,1 +1,15 @@
 //create theme
+export const theme = {
+    colors: {
+        navbarBgColor:"white",
+        mainColor:"#86C8BC",
+        logoColor:"darkslategray",
+        linkColor:"#8B7E74",
+        linkHoverColor:"rgb(5,5,5)",
+        headerFormColor:"rgba(15,63,42,0.36)"
+    },
+    screen:{
+        lg:"992px",
+    },
+
+}
