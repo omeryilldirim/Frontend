364206f0ff9e662dc71ebf3ec3c4cd88f9213f87
omeryilldirim <omeryilldirim@outlook.com>
2023-03-09 18:28:56 +0300

    my page

--- Teamwork-14/router-sass-project/src/pages/NotFound/NotFound.jsx
+++ Teamwork-14/router-sass-project/src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./NotFound.scss";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="notFound-container">
+      <img
+        className="object_astronaut"
+        src="http://salehriaz.com/404Page/img/astronaut.svg"
+        width="100px"
+        alt="astronaut"
+      />
+      <h1>404</h1>
+      <p>Looks like you are lost in the space!</p>
+      <button>Go Back</button>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
