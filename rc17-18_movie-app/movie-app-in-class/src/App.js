43c5095f310e96dbe536c57c515a0e0fa0619595
omeryilldirim <omeryilldirim@outlook.com>
2023-03-14 20:19:01 +0300

    movie app

--- rc17-18_movie-app/movie-app-in-class/src/App.js
+++ rc17-18_movie-app/movie-app-in-class/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import AuthContextProvider from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
+import { ToastContainer, toast } from 'react-toastify';
+
 
 const App = () => {
   return (
     <div>
       <AuthContextProvider>
         <AppRouter />
+        <ToastContainer />
       </AuthContextProvider>
     </div>
   );
--- rc17-18_movie-app/movie-app-in-class/src/index.js
+++ rc17-18_movie-app/movie-app-in-class/src/index.js
@@ -6,6 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import "tw-elements";
 import { BrowserRouter } from "react-router-dom";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
