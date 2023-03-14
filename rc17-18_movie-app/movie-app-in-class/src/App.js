5fa2a5ded63c52f4a2686204e8c72071f8baf111
omeryilldirim <omeryilldirim@outlook.com>
2023-03-14 21:43:33 +0300

    movie app

--- rc17-18_movie-app/movie-app-in-class/src/App.js
+++ rc17-18_movie-app/movie-app-in-class/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import AuthContextProvider from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
-import { ToastContainer } from 'react-toastify';
-
+import { ToastContainer } from "react-toastify";
+import MovieContextProvider from "./context/MovieContext";
 
 const App = () => {
   return (
     <div>
       <AuthContextProvider>
-        <AppRouter />
-        <ToastContainer />
+        <MovieContextProvider>
+          <AppRouter />
+          <ToastContainer />
+        </MovieContextProvider>
       </AuthContextProvider>
     </div>
   );
--- rc17-18_movie-app/movie-app-in-class/src/pages/Main.jsx
+++ rc17-18_movie-app/movie-app-in-class/src/pages/Main.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import MovieCard from "../components/MovieCard";
+import { MovieContext } from "../context/MovieContext";
 
 const Main = () => {
-  return <div>Main</div>;
+  const {movies} = useContext(MovieContext)
+
+  return <div>
+    <div className="flex justify-center  flex-wrap">
+      {movies.map((movie)=> <MovieCard key={movie.id} {...movie} />)}
+    </div>
+  </div>;
 };
 
 export default Main;
