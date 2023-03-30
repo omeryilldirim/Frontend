fdf1251636f8ba52dfd64c3b16b80d591ecd1275
omeryilldirim <omeryilldirim@outlook.com>
2023-03-31 02:12:17 +0300

    stock app

--- RC-23-28_stock-app/stock-app-inClass/src/pages/Home.jsx
+++ RC-23-28_stock-app/stock-app-inClass/src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import Typography from "@mui/material/Typography";
+import { useEffect } from "react";
+import Charts from "../components/Charts";
+import KPIcards from "../components/KPIcards";
+import useStockCall from "../hooks/useStockCall";
+
 const Home = () => {
-  return <div>Home</div>
+  const {getStockData} = useStockCall()
+
+  useEffect(() => {
+    getStockData("sales")
+    getStockData("purchases")
+  }, [])
+  
+  return <div>
+    <Typography variant="h4" color="error" mb={3}>Dashboard</Typography>
+    <KPIcards/>
+    <Charts/>
+  </div>
 }
 
 export default Home
